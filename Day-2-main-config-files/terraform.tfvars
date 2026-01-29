330d558e2b007b6806f807bc35e22181568b4d41
Ajith N <ajithnl314@gmail.com>
2026-01-29 14:05:00 +0530

    terradorm.tfvars

---
+++ Day-2-main-config-files/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_instance = "ami-055a9df0c8c9f681c"
+instance_type = "t2.micro"
--- Day-2-main-config-files/variables.tf
+++ Day-2-main-config-files/variables.tf
@@ -1,10 +1,10 @@
 variable "aws_instance" {
     type = string
-    default ="ami-055a9df0c8c9f681c" 
+    default ="" 
 }
 
 variable "instance_type" {
     type = string
-    default = "t2.micro"
+    default = ""
   
 }
