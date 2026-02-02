c56e97924cbd4129530c9da570f998d7876432b6
Ajith N <ajithnl314@gmail.com>
2026-02-02 19:20:18 +0530

    day-4

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "testdevjufgughuig"
     key    = "terraform.tfstate"
     region = "us-west-1"
+     use_lockfile = true # s3 supports this feature terraform version>1.10 latest version>=1.10
   }
 }
