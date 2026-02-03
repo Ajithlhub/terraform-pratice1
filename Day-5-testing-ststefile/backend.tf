91c3d96bb7a862a8ca7eaffb955af0a6b50311c1
Ajith N <ajithnl314@gmail.com>
2026-02-03 11:35:25 +0530

    day-5

---
+++ Day-5-testing-ststefile/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "testdevjufgughuig"
+    key    = "day-5/terraform.tfstate"
+    region = "us-west-1"
+     use_lockfile = true # s3 supports this feature terraform version>1.10 latest version>=1.10
+     #dynamodb_table = "test"#any versiong
+          encrypt = true
+  }
+}
