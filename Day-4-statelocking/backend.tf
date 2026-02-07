e946c6643915a7a885a62e61713f1a891f7711d8
Ajith N <ajithnl314@gmail.com>
2026-02-07 13:44:32 +0530

    day-4

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "testdevjufgughuig"
-    key    = "terraform.tfstate"
+    key    = "day-4/terraform.tfstate"
     region = "us-west-1"
-     #use_lockfile = true # s3 supports this feature terraform version>1.10 latest version>=1.10
-     dynamodb_table = "test"#any versiong
-          encrypt = true
+     use_lockfile = true # s3 supports this feature terraform version>1.10 latest version>=1.10
+     #dynamodb_table = "test"#any versiong
+          #encrypt = true
   }
 }
